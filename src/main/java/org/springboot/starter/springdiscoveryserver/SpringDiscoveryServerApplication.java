480b7541341da1bb31c09b004923a9cbfb305380
sandeep mishra <sandeepmishra023@gmail.com>
2018-12-24 16:14:15 +0530

    Eureka server initial commit

---
+++ src/main/java/org/springboot/starter/springdiscoveryserver/SpringDiscoveryServerApplication.java
@@ -0,0 +1,17 @@
+package org.springboot.starter.springdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiscoveryServerApplication.class, args);
+	}
+
+}
+
